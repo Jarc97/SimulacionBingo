eaac4516774ad87f0eeb465f4b22eb1f95028e85
Julio <julioandres97@gmail.com>
2017-10-08 04:14:58 -0600

    * Corrección de error en el constructor de la Matriz. Ahora se puede crear una Matriz de cualquier cantidad de filas y columnas.

--- SimulacionBingo/main.cpp
+++ SimulacionBingo/main.cpp
@@ -4,9 +4,9 @@
 #include "NodoMulti.h"
 
 int main() {
-	Matriz *m = new Matriz(2, 2);
+	Matriz *m = new Matriz(5, 5);
 	cout << NodoMulti::getCantidadNodos() << endl;
-	cout << m->toString() << endl;
+	// cout << m->toString() << endl;
 	system("PAUSE");
 	return 0;
 }
